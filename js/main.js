debfaa496fbaa66f717001a95b100eb61060d83b
Oleh Yatskiv <92208702+Oleh325@users.noreply.github.com>
2022-09-08 16:04:22 +0300

    Lab 2 (#2)
    * lab 2 responsiveness done
    
    * Some SICK animations done 🔥
    
    * hotfix
    
    * Fixed header scrolling bug
    
    * Fixed problems at 4k screen resolution
    
    * Hotfix + fixed readme
    
    * Small adjustments

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const OPEN_CLASSNAME = "open";
+
+const navLinks = document.getElementById("nav-links");
+
+function toggleMenu() {
+    navLinks.classList.toggle(OPEN_CLASSNAME);
+}
+
+var scroll = window.requestAnimationFrame ||
+            function(callback){ window.setTimeout(callback, 1000/60)};
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+    elementsToShow.forEach(function (element) {
+        if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+        }
+    });
+    scroll(loop);
+}
+
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
